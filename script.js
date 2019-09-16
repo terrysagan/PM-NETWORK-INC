160dce24ca4df62261934284b74a95721b4a5f00
terrysagan <55407619+terrysagan@users.noreply.github.com>
2019-09-16 12:01:40 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,385 @@
+﻿// CONTENT pest selection START
+
+
+// box 1
+//Get modal1 element
+var modal1 = document.getElementById('simpleModal1');
+// Get open modal button
+var modalBtn1 = document.getElementById('modalBtn1');
+//Get close button 
+var closeBtn1 = document.getElementsByClassName('closeBtn1')[0];
+
+// Listen for open click
+modalBtn1.addEventListener('click', openModal1);
+//Listen for close click
+closeBtn1.addEventListener('click', closeModal1);
+//Listen for outside click
+window.addEventListener('click', outsideClick);
+
+//Fucntion to open modal1
+function openModal1() {
+    modal1.style.display = 'block';
+}
+
+//Fucntion to close modal1
+function closeModal1() {
+    modal1.style.display = 'none';
+}
+
+//Fucntion to close modal1 if outside click
+function outsideClick(e) {
+    if (e.target == modal1) {
+        modal1.style.display = 'none';
+    }
+}
+// box 2
+//Get modal element
+var modal2 = document.getElementById('simpleModal2');
+// Get open modal button
+var modalBtn2 = document.getElementById('modalBtn2');
+//Get close button 
+var closeBtn2 = document.getElementsByClassName('closeBtn2')[0];
+
+// Listen for open click
+modalBtn2.addEventListener('click', openModal2);
+//Listen for close click
+closeBtn2.addEventListener('click', closeModal2);
+//Listen for outside click
+window.addEventListener('click', outsideClick);
+
+//Fucntion to open modal
+function openModal2() {
+    modal2.style.display = 'block';
+}
+
+//Fucntion to close modal
+function closeModal2() {
+    modal2.style.display = 'none';
+}
+
+//Fucntion to close modal if outside click
+function outsideClick(e) {
+    if (e.target == modal2) {
+        modal2.style.display = 'none';
+    }
+}
+// box 3
+//Get modal3 element
+var modal3 = document.getElementById('simpleModal3');
+// Get open modal button
+var modalBtn3 = document.getElementById('modalBtn3');
+//Get close button 
+var closeBtn3 = document.getElementsByClassName('closeBtn3')[0];
+
+// Listen for open click
+modalBtn3.addEventListener('click', openModal3);
+//Listen for close click
+closeBtn3.addEventListener('click', closeModal3);
+//Listen for outside click
+window.addEventListener('click', outsideClick);
+
+//Fucntion to open modal3
+function openModal3() {
+    modal3.style.display = 'block';
+}
+
+//Fucntion to close modal3
+function closeModal3() {
+    modal3.style.display = 'none';
+}
+
+//Fucntion to close modal3 if outside click
+function outsideClick(e) {
+    if (e.target == modal3) {
+        modal3.style.display = 'none';
+    }
+}
+// box 4
+//Get modal4 element
+var modal4 = document.getElementById('simpleModal4');
+// Get open modal button
+var modalBtn4 = document.getElementById('modalBtn4');
+//Get close button 
+var closeBtn4 = document.getElementsByClassName('closeBtn4')[0];
+
+// Listen for open click
+modalBtn4.addEventListener('click', openModal4);
+//Listen for close click
+closeBtn4.addEventListener('click', closeModal4);
+//Listen for outside click
+window.addEventListener('click', outsideClick);
+
+//Fucntion to open modal4
+function openModal4() {
+    modal4.style.display = 'block';
+}
+
+//Fucntion to close modal4
+function closeModal4() {
+    modal4.style.display = 'none';
+}
+
+//Fucntion to close modal4 if outside click
+function outsideClick(e) {
+    if (e.target == modal4) {
+        modal4.style.display = 'none';
+    }
+}
+
+// box 5
+//Get modal5 element
+var modal5 = document.getElementById('simpleModal5');
+// Get open modal button
+var modalBtn5 = document.getElementById('modalBtn5');
+//Get close button 
+var closeBtn5 = document.getElementsByClassName('closeBtn5')[0];
+
+// Listen for open click
+modalBtn5.addEventListener('click', openModal5);
+//Listen for close click
+closeBtn5.addEventListener('click', closeModal5);
+//Listen for outside click
+window.addEventListener('click', outsideClick);
+
+//Fucntion to open modal5
+function openModal5() {
+    modal5.style.display = 'block';
+}
+
+//Fucntion to close modal5
+function closeModal5() {
+    modal5.style.display = 'none';
+}
+
+//Fucntion to close modal4 if outside click
+function outsideClick(e) {
+    if (e.target == modal5) {
+        modal5.style.display = 'none';
+    }
+}
+// box 6
+//Get modal6 element
+var modal6 = document.getElementById('simpleModal6');
+// Get open modal button
+var modalBtn6 = document.getElementById('modalBtn6');
+//Get close button 
+var closeBtn6 = document.getElementsByClassName('closeBtn6')[0];
+
+// Listen for open click
+modalBtn6.addEventListener('click', openModal6);
+//Listen for close click
+closeBtn6.addEventListener('click', closeModal6);
+//Listen for outside click
+window.addEventListener('click', outsideClick);
+
+//Fucntion to open modal6
+function openModal6() {
+    modal6.style.display = 'block';
+}
+
+//Fucntion to close modal6
+function closeModal6() {
+    modal6.style.display = 'none';
+}
+
+//Fucntion to close modal5 if outside click
+function outsideClick(e) {
+    if (e.target == modal6) {
+        modal6.style.display = 'none';
+    }
+}
+
+
+// box 7
+//Get modal7 element
+var modal7 = document.getElementById('simpleModal7');
+// Get open modal button
+var modalBtn7 = document.getElementById('modalBtn7');
+//Get close button 
+var closeBtn7 = document.getElementsByClassName('closeBtn7')[0];
+
+// Listen for open click
+modalBtn7.addEventListener('click', openModal7);
+//Listen for close click
+closeBtn7.addEventListener('click', closeModal7);
+//Listen for outside click
+window.addEventListener('click', outsideClick);
+
+//Fucntion to open modal6
+function openModal7() {
+    modal7.style.display = 'block';
+}
+
+//Fucntion to close modal5
+function closeModal7() {
+    modal7.style.display = 'none';
+}
+
+//Fucntion to close modal5 if outside click
+function outsideClick(e) {
+    if (e.target == modal7) {
+        modal7.style.display = 'none';
+    }
+}
+
+// box 8
+//Get modal8 element
+var modal8 = document.getElementById('simpleModal8');
+// Get open modal button
+var modalBtn8 = document.getElementById('modalBtn8');
+//Get close button 
+var closeBtn8 = document.getElementsByClassName('closeBtn8')[0];
+
+// Listen for open click
+modalBtn8.addEventListener('click', openModal8);
+//Listen for close click
+closeBtn8.addEventListener('click', closeModal8);
+//Listen for outside click
+window.addEventListener('click', outsideClick);
+
+//Fucntion to open modal6
+function openModal8() {
+    modal8.style.display = 'block';
+}
+
+//Fucntion to close modal5
+function closeModal8() {
+    modal8.style.display = 'none';
+}
+
+//Fucntion to close modal5 if outside click
+function outsideClick(e) {
+    if (e.target == modal8) {
+        modal8.style.display = 'none';
+    }
+}
+
+// box 9
+//Get modal9 element
+var modal9 = document.getElementById('simpleModal9');
+// Get open modal button
+var modalBtn9 = document.getElementById('modalBtn9');
+//Get close button 
+var closeBtn9 = document.getElementsByClassName('closeBtn9')[0];
+
+// Listen for open click
+modalBtn9.addEventListener('click', openModal9);
+//Listen for close click
+closeBtn9.addEventListener('click', closeModal9);
+//Listen for outside click
+window.addEventListener('click', outsideClick);
+
+//Fucntion to open modal9
+function openModal9() {
+    modal9.style.display = 'block';
+}
+
+//Fucntion to close modal9
+function closeModal9() {
+    modal9.style.display = 'none';
+}
+
+//Fucntion to close modal9 if outside click
+function outsideClick(e) {
+    if (e.target == modal9) {
+        modal9.style.display = 'none';
+    }
+}
+
+// box 10
+//Get modal10 element
+var modal10 = document.getElementById('simpleModal10');
+// Get open modal button
+var modalBtn10 = document.getElementById('modalBtn10');
+//Get close button 
+var closeBtn10 = document.getElementsByClassName('closeBtn10')[0];
+
+// Listen for open click
+modalBtn10.addEventListener('click', openModal10);
+//Listen for close click
+closeBtn10.addEventListener('click', closeModal10);
+//Listen for outside click
+window.addEventListener('click', outsideClick);
+
+//Fucntion to open modal10
+function openModal10() {
+    modal10.style.display = 'block';
+}
+
+//Fucntion to close modal10
+function closeModal10() {
+    modal10.style.display = 'none';
+}
+
+//Fucntion to close modal10 if outside click
+function outsideClick(e) {
+    if (e.target == modal10) {
+        modal10.style.display = 'none';
+    }
+}
+
+// box 11
+//Get modal10 element
+var modal11 = document.getElementById('simpleModal11');
+// Get open modal button
+var modalBtn11 = document.getElementById('modalBtn11');
+//Get close button 
+var closeBtn11 = document.getElementsByClassName('closeBtn11')[0];
+
+// Listen for open click
+modalBtn11.addEventListener('click', openModal11);
+//Listen for close click
+closeBtn11.addEventListener('click', closeModal11);
+//Listen for outside click
+window.addEventListener('click', outsideClick);
+
+//Fucntion to open modal11
+function openModal11() {
+    modal11.style.display = 'block';
+}
+
+//Fucntion to close modal11
+function closeModal11() {
+    modal11.style.display = 'none';
+}
+
+//Fucntion to close modal10 if outside click
+function outsideClick(e) {
+    if (e.target == modal11) {
+        modal11.style.display = 'none';
+    }
+}
+
+// box 12
+//Get modal12 element
+var modal12 = document.getElementById('simpleModal12');
+// Get open modal button
+var modalBtn12 = document.getElementById('modalBtn12');
+//Get close button 
+var closeBtn12 = document.getElementsByClassName('closeBtn12')[0];
+
+// Listen for open click
+modalBtn12.addEventListener('click', openModal12);
+//Listen for close click
+closeBtn12.addEventListener('click', closeModal12);
+//Listen for outside click
+window.addEventListener('click', outsideClick);
+
+//Fucntion to open modal12
+function openModal12() {
+    modal12.style.display = 'block';
+}
+
+//Fucntion to close modal12
+function closeModal12() {
+    modal12.style.display = 'none';
+}
+
+//Fucntion to close modal12 if outside click
+function outsideClick(e) {
+    if (e.target == modal12) {
+        modal12.style.display = 'none';
+    }
+}
+
+// CONTENT pest selection END
